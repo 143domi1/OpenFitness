789b368bcf9790ff266505a0a222dbf59d0c3234
Dominik <93765669+143domi1@users.noreply.github.com>
2026-05-29 17:41:04 +0200

    Added code

---
+++ main.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <fstream>
+using json = nlohmann::json;
+using namespace std;
+
+int command();
+json we; // This is for the weight    
+json wat; // This is for the water 
+int main() {
+    json cf = {
+    {"verify", 0}
+};
+
+    ifstream Conf("conf.json");
+    if (Conf.is_open()) {
+        try {
+            Conf >> cf;
+
+        } catch(...) {
+            cf = {{"verify", 0}};
+        }
+        Conf.close();
+    }
+
+    int verif = cf.value("verify", 0);
+
+    if (verif==0) {
+        //Tutorial 
+        cout << "Hello! Welcome to OpenFitness (Beta)\n" << "OpenFitness is foss (free and open source) fitness application!\n";
+        cout << "For your convenience, did you use this app before?\n" << "If yes, you are free to skip this tutorial.\n" << "Y - yes, N - no\n";
+        char yn;
+        cin >> yn;
+        if (yn=='Y') {
+            command();
+        } else if (yn=='N') {
+            cout << "Hello! Since this app is currently in BETA,\n" << "it sadly only supports water intake tracker and weight input tracker.\n" << "So welcome! :)\n";
+            verif = 1;
+            cf["verify"]=verif;
+            ofstream conf("conf.json");
+            conf << cf.dump(4);
+            conf.close();
+            //call command function here
+            command();
+        }
+     } else {
+        command();
+        //cout << "nice :)";
+     }
+    return 0;
+}
+
+int command() {
+    json we = json::object();
+
+    ifstream infile("weight.json");
+    if (infile.is_open()) {
+        try {
+            infile >> we;
+        } catch (...) {
+            we = json::object();
+        }
+        infile.close();
+    }
+    json wat = json::object();
+
+    ifstream WaterIn("water.json");
+    if (WaterIn.is_open()) {
+        try {
+            WaterIn >> wat;
+        } catch (...) {
+            wat = json::object();
+        }
+        WaterIn.close();
+    }
+    cout << "What do you want to do?\n" << "1. Input weight.\n2. Input water.\n" ;
+    int ot;
+    cin >> ot;
+    if (ot==1) {
+        cout << "Please input the weight (kg): ";
+        float w;
+        cin >> w;
+        cout << "Please enter the date (DD-MM-YYYY): ";
+        string date;
+        cin >> date;
+        we[date]["Date"]=date;
+        we[date]["weight"]=w;
+        ofstream outfile("weight.json");
+        outfile << we.dump(4);
+        outfile.close();
+        command();
+    } else if (ot==2) {
+        cout << "Please input the amount you drank (ml): ";
+        float wa;
+        cin >> wa;
+        cout << "Please enter the date (DD-MM-YYYY): ";
+        string time;
+        cin >> time;
+        wat[time]["Date"]=time;
+        wat[time]["Water (ml)"]=wa;
+        ofstream file("water.json");
+        file << wat.dump(4);
+        file.close();
+        command();
+    }
+    return 0;
+}
+
+/*Program is under the licence of GNU General Public License V3
+This text MUST NOT be removed.*/
