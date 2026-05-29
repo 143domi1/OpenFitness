9dc8eb72133ddb216a45210ff2e81c0e2abe1595
Dominik <93765669+143domi1@users.noreply.github.com>
2026-05-29 21:47:29 +0200

    Updated code with steps.
    Added new steps tracking function

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int command();
 json we; // This is for the weight    
 json wat; // This is for the water 
+json st; // This is for the steps
 int main() {
     json cf = {
     {"verify", 0}
@@ -34,7 +35,7 @@ int main() {
         if (yn=='Y') {
             command();
         } else if (yn=='N') {
-            cout << "Hello! Since this app is currently in BETA,\n" << "it sadly only supports water intake tracker and weight input tracker.\n" << "So welcome! :)\n";
+            cout << "Hello! Since this app is currently in BETA,\n" << "it sadly only supports water tracking, weight tracking and steps tracker.\n" << "So welcome! :)\n";
             verif = 1;
             cf["verify"]=verif;
             ofstream conf("conf.json");
@@ -73,7 +74,19 @@ int command() {
         }
         WaterIn.close();
     }
-    cout << "What do you want to do?\n" << "1. Input weight.\n2. Input water.\n" ;
+
+    json steps = json::object();
+
+    ifstream StepsIn("steps.json");
+    if (StepsIn.is_open()) {
+        try {
+            StepsIn >> st;
+        } catch (...) {
+            st = json::object();
+        }
+        StepsIn.close();
+    }
+    cout << "What do you want to do?\n" << "1. Input weight.\n2. Input water.\n3. Input steps.\n" ;
     int ot;
     cin >> ot;
     if (ot==1) {
@@ -101,6 +114,19 @@ int command() {
         ofstream file("water.json");
         file << wat.dump(4);
         file.close();
+        command();
+    } else if (ot==3) {
+        cout << "Please input the amount of steps you did (the specific day, NO comma or seperator): ";
+        int steps;
+        cin >> steps;
+        cout << "Please enter the date (DD-MM-YYYY): ";
+        string date;
+        cin >> date;
+        st[date]["Date"]=date;
+        st[date]["Steps"]=steps;
+        ofstream sfile("steps.json");
+        sfile << st.dump(4);
+        sfile.close();
         command();
     }
     return 0;
